aeedcaf53c38be2390c19db405926a6fe54de73c
droneblack01-source <droneblack01@gmail.com>
2025-11-05 16:54:24 -0300

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,7 @@
+nome = input("Seu nome?")
+print("olá", nome)
+idade("qual sua idade?")
+ if idade<18:
+    print(" não pode trabalhar ainda infelizmente kk")
+ else: 
+    print("já pode trabalhar")
