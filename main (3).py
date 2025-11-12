003688e819c054c12ee1b68b088af69f9320d031
droneblack01-source <droneblack01@gmail.com>
2025-11-12 16:09:25 -0300

    jogo de perguntas

---
+++ main (3).py
@@ -0,0 +1,25 @@
+nome = input("Seu nome? ")
+print(" olá", nome)
+idade = int(input("qual sua idade?"))
+ 
+if idade < 18:
+    print(" não pode trabalhar ainda infelizmente kk")
+else: 
+    print("já pode trabalhar")
+    
+trabalho = input("Só aproveitando a deixa, vou fazer uma charada pra você de dia estou limpo de noite estou preto qual a minha profissão?")
+   
+if trabalho == "Mecanico" or trabalho == "mecanico":
+    print("acertou kk")
+else:
+    print("errou -_-")
+
+i = input("Só vou fazer mais uma, digite um numero:")
+
+if i == 8:
+    print("acertou o numero ae")
+else:
+    print("errou")
+   
+   
+   
